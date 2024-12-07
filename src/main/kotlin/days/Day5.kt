01361eabf6f2cd1b491f04781d267130d9a45616
Andreas Lau <andilau@users.noreply.github.com>
2024-12-07 20:05:13 +0100

    Day 5: Print Queue (#5)

---
+++ src/main/kotlin/days/Day5.kt
@@ -0,0 +1,47 @@
+package days
+
+@AdventOfCodePuzzle(
+    name = "Print Queue",
+    url = "https://adventofcode.com/2024/day/5",
+    date = Date(day = 5, year = 2024)
+)
+class Day5(input: List<String>) : Puzzle {
+
+    private val pageOrderingRules = input
+        .takeWhile { it.isNotEmpty() }
+        .map { line -> line.split('|').let { it[0].toInt() to it[1].toInt() } }
+    private val pageToProduce = input
+        .dropWhile { it.isNotEmpty() }
+        .drop(1)
+        .map { it.split(',').map { it.toInt() } }
+
+    override fun partOne(): Int = pageToProduce
+        .filter { pages -> validOrder(pages) }
+        .sumOf(::middlePage)
+
+    override fun partTwo() = pageToProduce
+        .filterNot { pages -> validOrder(pages) }
+        .map { pages -> reorder(pages) }
+        .sumOf { middlePage(it) }
+
+    private fun reorder(pages: List<Int>): List<Int> {
+        val selectedRules = pageOrderingRules.filter { (prev, next) -> prev in pages && next in pages }
+        val eachCount = selectedRules.groupingBy { it.first }.eachCount().toMutableMap()
+        selectedRules.map { it.second }.forEach { eachCount.putIfAbsent(it, 0) }
+
+        return eachCount
+            .map { (page, degree) -> degree to page }
+            .toMap()
+            .toSortedMap(Comparator.naturalOrder<Int?>().reversed())
+            .values.toList()
+    }
+
+    private fun validOrder(pages: List<Int>): Boolean {
+        return pageOrderingRules
+            .filter { (prev, next) -> prev in pages && next in pages }
+            .all { (prev, next) -> pages.indexOf(prev) < pages.indexOf(next) }
+    }
+
+    private fun middlePage(it: List<Int>) = it[it.size / 2]
+
+}
--- src/test/kotlin/SolutionsTest.kt
+++ src/test/kotlin/SolutionsTest.kt
@@ -1,7 +1,4 @@
-import days.Day1
-import days.Day2
-import days.Day3
-import days.Day4
+import days.*
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.DynamicTest
@@ -16,6 +13,7 @@ class SolutionsTest {
         Day2(InputReader.getInputAsList(2)) to Pair(559, 601),
         Day3(InputReader.getInputAsList(3)) to Pair(178886550, 87163705),
         Day4(InputReader.getInputAsList(4)) to Pair(2414, 1871),
+        Day5(InputReader.getInputAsList(5)) to Pair(6041, 4884),
     )
         .map { (day, answers) ->
             DynamicTest.dynamicTest("${day.javaClass.simpleName} -> ${answers.first} / ${answers.second}") {
