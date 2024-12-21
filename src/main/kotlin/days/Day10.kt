fa5ed75f2e357661ee523d8bd460ea6ba31cba93
Andreas Lau <andreas.lau@europace.de>
2024-12-21 14:18:05 +0100

    Day 10: Hoof It

--- src/main/kotlin/days/Day10.kt
+++ src/main/kotlin/days/Day10.kt
@@ -1,5 +1,7 @@
 package days
 
+import java.util.PriorityQueue
+
 @AdventOfCodePuzzle(
     name = "Hoof It",
     url = "https://adventofcode.com/2024/day/10",
@@ -7,21 +9,50 @@ package days
 )
 class Day10(val input: List<String>) : Puzzle {
 
-    val sym: Map<Point, Int> = findAllIgnoring().toMap()
+    val map: Map<Point, Int> = findAllIgnoring().toMap()
+    val starts: Set<Point> = map.filter { (_, v) -> v == 0 }.keys
+    val exits: Set<Point> = map.filter { (_, v) -> v == 9 }.keys
 
-    override fun partOne(): Int {
-        val starts = sym.filter { (_, v) -> v == 0 }.keys
-        return starts.sumOf(::findPath)
-    }
+    override fun partOne() = starts.sumOf { start -> scoreTrail(start, exits) }
 
-    private fun findPath(start: Point): Int {
-        println("start = $start")
-        return 1
-    }
-
-    override fun partTwo() = 0
+    override fun partTwo() = starts.sumOf { start -> scoreTrail2(start, exits) }
 
     private fun findAllIgnoring(): List<Pair<Point, Int>> = input
         .flatMapIndexed { y, line -> line.mapIndexedNotNull() { x, c -> if (c.isDigit()) Point(x, y) to c.digitToInt() else null } }
 
+    private fun scoreTrail(start: Point, exits: Set<Point>): Int {
+        var reached = mutableSetOf<Point>()
+        val queue = PriorityQueue<Pair<Point, Int>> { a, b -> a.second.compareTo(b.second) }
+        queue.add(start to 0)
+        val seen = mutableSetOf<Point>()
+
+        while (queue.isNotEmpty()) {
+            val (next, cost) = queue.poll()
+
+            if (next in seen) continue
+            seen += next
+            if (next in exits) reached += next
+            next.neighbors()
+                .filter { it in map }
+                .filter { map[it]!! - map[next]!! == 1 }
+                .forEach { queue.add(it to cost + 1) }
+        }
+        return reached.size
+    }
+
+    private fun scoreTrail2(start: Point, exits: Set<Point>): Int {
+        var reached = mutableListOf<Point>()
+        val queue = PriorityQueue<List<Point>> { a, b -> a.size.compareTo(b.size) }
+        queue.add(listOf(start))
+        while (queue.isNotEmpty()) {
+            val path = queue.poll()
+
+            if (path.last() in exits) reached += path.last()
+            path.last().neighbors()
+                .filter { it in map }
+                .filter { map[it]!! - map[path.last()]!! == 1 }
+                .forEach { queue.add(path + it) }
+        }
+        return reached.size
+    }
 }
--- src/test/kotlin/days/Day10Test.kt
+++ src/test/kotlin/days/Day10Test.kt
@@ -72,10 +72,53 @@ class Day10Test {
     @Nested
     @DisplayName("Part 2")
     inner class Part2 {
-        @Test
-        fun `How many unique locations within the bounds of the map contain an antinode`() {
-            assertThat(Day8(trailheadScore1).partTwo()).isEqualTo(34)
-        }
 
+        val trailheadScore6 = """
+            .....0.
+            ..4321.
+            ..5..2.
+            ..6543.
+            ..7..4.
+            ..8765.
+            ..9....""".trimIndent().lines()
+
+        val trailheadScore7 = """
+            ..90..9
+            ...1.98
+            ...2..7
+            6543456
+            765.987
+            876....
+            987....""".trimIndent().lines()
+
+        val trailheadScore8 = """
+            012345
+            123456
+            234567
+            345678
+            4.6789
+            56789.""".trimIndent().lines()
+        val trailheadScore9 = """
+            89010123
+            78121874
+            87430965
+            96549874
+            45678903
+            32019012
+            01329801
+            10456732""".trimIndent().lines()
+
+        @TestFactory
+        fun `What is the sum of the scores of all trailheads on your topographic map`() =
+            listOf(
+                trailheadScore6 to 3,
+                trailheadScore7 to 13,
+                trailheadScore8 to 227,
+                trailheadScore9 to 81,
+            ).map { (map, score) ->
+                DynamicTest.dynamicTest("The sum of the scores of all trailheads on the topographic map is $score") {
+                    assertThat(Day10(map).partTwo()).isEqualTo(score)
+                }
+            }
     }
 }
