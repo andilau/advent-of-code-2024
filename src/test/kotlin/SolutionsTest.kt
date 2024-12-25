9a0bc181cf683a52ef52221c94db52206864ff52
Andi Lau <andi@herrlau.de>
2024-12-25 16:21:40 +0100

    Day 25: Code Chronicle

--- src/test/kotlin/SolutionsTest.kt
+++ src/test/kotlin/SolutionsTest.kt
@@ -21,6 +21,7 @@ class SolutionsTest {
         Day14(InputReader.getInputAsList(14)) to Pair(214109808L, 7687),
         Day18(InputReader.getInputAsList(18)) to Pair(370, "65,6"),
         Day22(InputReader.getInputAsList(22)) to Pair(14869099597L, 1717),
+        Day25(InputReader.getInputAsString(25)) to Pair(3223, Unit),
     )
         .map { (day, answers) ->
             DynamicTest.dynamicTest("${day.javaClass.simpleName} -> ${answers.first} / ${answers.second}") {
---
+++ src/test/kotlin/days/Day25Test.kt
@@ -0,0 +1,63 @@
+package days
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Disabled
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+
+@DisplayName("Day 25")
+class Day25Test {
+    val schematics = """
+        #####
+        .####
+        .####
+        .####
+        .#.#.
+        .#...
+        .....
+        
+        #####
+        ##.##
+        .#.##
+        ...##
+        ...#.
+        ...#.
+        .....
+        
+        .....
+        #....
+        #....
+        #...#
+        #.#.#
+        #.###
+        #####
+        
+        .....
+        .....
+        #.#..
+        ###..
+        ###.#
+        ###.#
+        #####
+        
+        .....
+        .....
+        .....
+        #....
+        #.#..
+        #.#.#
+        #####""".trimIndent()
+
+    @Nested
+    @DisplayName("Part 1")
+    inner class Part1 {
+
+        @Test
+        fun `How many unique lock-key pairs fit together without overlapping in any column`() {
+            assertThat(Day25(schematics).partOne()).isEqualTo(3)
+        }
+
+    }
+
+}
